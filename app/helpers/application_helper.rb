0bf849089eb0a750bd6d1af38f1db1b5cfbd960a
razor <razorcd@yahoo.com>
2015-10-11 14:59:32 +0300

    es6_inline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def es6_transform(es6_code = nil, &block)
+    es6_code ||= capture(&block)
+    js_code = Babel::Transpiler.transform(es6_code)["code"]
+    js_code.html_safe #js code
+    # javascript_tag js_code #js code with script tags
+  end
 end
--- app/views/messages/_test.js.es6
+++ app/views/messages/_test.js.es6
@@ -1,2 +1,2 @@
-let <%='x'%> = "ES6!!!!";
-alert(x)
+let x = "<%= message %>";
+console.log(x)
